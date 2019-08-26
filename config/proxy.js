9d15e7516ae2ae44cc8c59f95fa1bbef9cc3c620
Liam Potter <me@liam-potter.co.uk>
2019-08-26 11:30:16 +0100

    fix proxy?

--- config/proxy.js
+++ config/proxy.js
@@ -3,8 +3,13 @@ export default {
     pathRewrite: {
       '^/api': '/api/v1',
     },
+
     target: (process.env.NODE_ENV === 'production')
       ? 'https://api.thehiddenspace.co.uk'
       : 'http://localhost:3333',
+
+    headers: {
+      'HiddenSpace-Api-Key': process.env.API_KEY,
+    },
   },
 }
